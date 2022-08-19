13313ba53e948dfed37b4f44ba1ead6522751116
Guillaume Baptist <guillaume.baptist@free.fr>
2022-08-19 20:47:52 +0200

    minimalist code working with still the intterupt

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,3 @@
-/********************************************************
-*
-* Project: MAXREFDES117#
-* Filename: RD117_ARDUINO.ino
-* Description: This module contains the Main application for the MAXREFDES117 example program.
-*
-* Revision History:
-*\n 1-18-2016 Rev 01.00 GL Initial release.
-*\n 12-22-2017 Rev 02.00 Significantlly modified by Robert Fraczkiewicz
-*\n 08-22-2018 Rev 02.01 Added conditional compilation of the code related to ADALOGGER SD card operations
-*
-* --------------------------------------------------------------------
-*
-* This code follows the following naming conventions:
-*
-* char              ch_pmod_value
-* char (array)      s_pmod_s_string[16]
-* float             f_pmod_value
-* int32_t           n_pmod_value
-* int32_t (array)   an_pmod_value[16]
-* int16_t           w_pmod_value
-* int16_t (array)   aw_pmod_value[16]
-* uint16_t          uw_pmod_value
-* uint16_t (array)  auw_pmod_value[16]
-* uint8_t           uch_pmod_value
-* uint8_t (array)   auch_pmod_buffer[16]
-* uint32_t          un_pmod_value
-* int32_t *         pn_pmod_value
-*
-* ------------------------------------------------------------------------- */
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -44,6 +14,8 @@ uint32_t aun_red_buffer[BUFFER_SIZE];  //red LED sensor data
 float old_n_spo2;  // Previous SPO2 value
 uint8_t uch_dummy,k;
 
+String Data;
+
 
 void millis_to_hours(uint32_t ms, char* hr_str);
 
@@ -56,17 +28,13 @@ void setup() {
   maxim_max30102_init();  //initialize the MAX30102
   old_n_spo2=0.0;
 
-  while(Serial.available()==0)  //wait until user presses a key
-  {
-    Serial.println(F("Press any key to start conversion"));
-    delay(1000);
-  }
-  uch_dummy=Serial.read();
+  // while(Serial.available()==0)  //wait until user presses a key
+  // {
+  //   Serial.println(F("Press any key to start conversion"));
+  //   delay(1000);
+  // }
+  // uch_dummy=Serial.read();
 
-  Serial.print(F("Time[s]\tSpO2\tHR\tClock\tRatio\tCorr\tTemp[C]"));
-
-  Serial.println("");
-  
   timeStart=millis();
 }
 
@@ -84,8 +52,8 @@ void loop() {
   for(i=0;i<BUFFER_SIZE;i++)
   {
     while(digitalRead(oxiInt)==1);  //wait until the interrupt pin asserts
-    //maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
-    // change / inverted from Guillaume
+
+    // here is the switch IR/RED due to hardware defect (Guillaume)
     maxim_max30102_read_fifo((aun_ir_buffer + i), (aun_red_buffer + i)); //read from MAX30102 FIFO
 
   }
@@ -93,52 +61,16 @@ void loop() {
   //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using Robert's method
   rf_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid, &ratio, &correl); 
   elapsedTime=millis()-timeStart;
-  millis_to_hours(elapsedTime,hr_str); // Time in hh:mm:ss format
-  elapsedTime/=1000; // Time in seconds
 
-  // Read the _chip_ temperature in degrees Celsius
-  int8_t integer_temperature;
-  uint8_t fractional_temperature;
-  maxim_max30102_read_temperature(&integer_temperature, &fractional_temperature);
-  float temperature = integer_temperature + ((float)fractional_temperature)/16.0;
+  elapsedTime/=1000; // Time in seconds
 
   if(ch_hr_valid && ch_spo2_valid) { 
 
-    Serial.print(elapsedTime);
-    Serial.print("\t");
-    Serial.print(n_spo2);
-    Serial.print("\t");
-    Serial.print(n_heart_rate, DEC);
-    Serial.print("\t");
+    Data=String(String(elapsedTime) + ',' + String(n_spo2) + ','  + String(n_heart_rate) + ',');
 
-    Serial.print(hr_str);
-    Serial.print("\t");
-    Serial.print(ratio);
-    Serial.print("\t");
-    Serial.print(correl);
-    Serial.print("\t");
-    Serial.print(temperature);
-
-    Serial.println("");
+    Serial.println(Data);
 
     old_n_spo2=n_spo2;
   }
 }
 
-void millis_to_hours(uint32_t ms, char* hr_str)
-{
-  char istr[6];
-  uint32_t secs,mins,hrs;
-  secs=ms/1000; // time in seconds
-  mins=secs/60; // time in minutes
-  secs-=60*mins; // leftover seconds
-  hrs=mins/60; // time in hours
-  mins-=60*hrs; // leftover minutes
-  itoa(hrs,hr_str,10);
-  strcat(hr_str,":");
-  itoa(mins,istr,10);
-  strcat(hr_str,istr);
-  strcat(hr_str,":");
-  itoa(secs,istr,10);
-  strcat(hr_str,istr);
-}
