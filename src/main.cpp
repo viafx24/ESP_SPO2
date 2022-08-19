28444494af946aafd5be06cacec4f0a178d0c041
Guillaume Baptist <guillaume.baptist@free.fr>
2022-08-19 20:19:10 +0200

    before removing useless code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,150 @@
+// /********************************************************
+// *
+// * Project: MAXREFDES117#
+// * Filename: RD117_ARDUINO.ino
+// * Description: This module contains the Main application for the MAXREFDES117 example program.
+// *
+// * Revision History:
+// *\n 1-18-2016 Rev 01.00 GL Initial release.
+// *\n 12-22-2017 Rev 02.00 Significantlly modified by Robert Fraczkiewicz
+// *\n 08-22-2018 Rev 02.01 Added conditional compilation of the code related to ADALOGGER SD card operations
+// *
+// * --------------------------------------------------------------------
+// *
+// * This code follows the following naming conventions:
+// *
+// * char              ch_pmod_value
+// * char (array)      s_pmod_s_string[16]
+// * float             f_pmod_value
+// * int32_t           n_pmod_value
+// * int32_t (array)   an_pmod_value[16]
+// * int16_t           w_pmod_value
+// * int16_t (array)   aw_pmod_value[16]
+// * uint16_t          uw_pmod_value
+// * uint16_t (array)  auw_pmod_value[16]
+// * uint8_t           uch_pmod_value
+// * uint8_t (array)   auch_pmod_buffer[16]
+// * uint32_t          un_pmod_value
+// * int32_t *         pn_pmod_value
+// *
+// * ------------------------------------------------------------------------- */
+
+// #include <Arduino.h>
+// #include <algorithm_by_RF.h>
+// #include <max30102.h>
+// #include <SPI.h>
+
+
+// // Interrupt pin
+// const byte oxiInt = 19; // pin connected to MAX30102 INT
+
+// uint32_t elapsedTime,timeStart;
+
+// uint32_t aun_ir_buffer[BUFFER_SIZE]; //infrared LED sensor data
+// uint32_t aun_red_buffer[BUFFER_SIZE];  //red LED sensor data
+// float old_n_spo2;  // Previous SPO2 value
+// uint8_t uch_dummy,k;
+
+
+// void millis_to_hours(uint32_t ms, char* hr_str);
+
+// void setup() {
+
+//   pinMode(oxiInt, INPUT);  //pin D10 connects to the interrupt output pin of the MAX30102
+
+//   Serial.begin(115200);
+
+//   maxim_max30102_init();  //initialize the MAX30102
+//   old_n_spo2=0.0;
+
+//   while(Serial.available()==0)  //wait until user presses a key
+//   {
+//     Serial.println(F("Press any key to start conversion"));
+//     delay(1000);
+//   }
+//   uch_dummy=Serial.read();
+
+//   Serial.print(F("Time[s]\tSpO2\tHR\tClock\tRatio\tCorr\tTemp[C]"));
+
+//   Serial.println("");
+  
+//   timeStart=millis();
+// }
+
+// //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every ST seconds
+// void loop() {
+//   float n_spo2,ratio,correl;  //SPO2 value
+//   int8_t ch_spo2_valid;  //indicator to show if the SPO2 calculation is valid
+//   int32_t n_heart_rate; //heart rate value
+//   int8_t  ch_hr_valid;  //indicator to show if the heart rate calculation is valid
+//   int32_t i;
+//   char hr_str[10];
+     
+//   //buffer length of BUFFER_SIZE stores ST seconds of samples running at FS sps
+//   //read BUFFER_SIZE samples, and determine the signal range
+//   for(i=0;i<BUFFER_SIZE;i++)
+//   {
+//     while(digitalRead(oxiInt)==1);  //wait until the interrupt pin asserts
+//     //maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
+
+//     // change / inverted from Guillaume
+//     maxim_max30102_read_fifo((aun_ir_buffer + i), (aun_red_buffer + i)); //read from MAX30102 FIFO
+
+//   }
+
+//   //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using Robert's method
+//   rf_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid, &ratio, &correl); 
+//   elapsedTime=millis()-timeStart;
+//   millis_to_hours(elapsedTime,hr_str); // Time in hh:mm:ss format
+//   elapsedTime/=1000; // Time in seconds
+
+//   // Read the _chip_ temperature in degrees Celsius
+//   int8_t integer_temperature;
+//   uint8_t fractional_temperature;
+//   maxim_max30102_read_temperature(&integer_temperature, &fractional_temperature);
+//   float temperature = integer_temperature + ((float)fractional_temperature)/16.0;
+
+//   if(ch_hr_valid && ch_spo2_valid) { 
+
+//     Serial.print(elapsedTime);
+//     Serial.print("\t");
+//     Serial.print(n_spo2);
+//     Serial.print("\t");
+//     Serial.print(n_heart_rate, DEC);
+//     Serial.print("\t");
+//     Serial.print(hr_str);
+//     Serial.print("\t");
+//     Serial.print(ratio);
+//     Serial.print("\t");
+//     Serial.print(correl);
+//     Serial.print("\t");
+//     Serial.print(temperature);
+
+//     Serial.println("");
+//     old_n_spo2=n_spo2;
+//   }
+// }
+
+// void millis_to_hours(uint32_t ms, char* hr_str)
+// {
+//   char istr[6];
+//   uint32_t secs,mins,hrs;
+//   secs=ms/1000; // time in seconds
+//   mins=secs/60; // time in minutes
+//   secs-=60*mins; // leftover seconds
+//   hrs=mins/60; // time in hours
+//   mins-=60*hrs; // leftover minutes
+//   itoa(hrs,hr_str,10);
+//   strcat(hr_str,":");
+//   itoa(mins,istr,10);
+//   strcat(hr_str,istr);
+//   strcat(hr_str,":");
+//   itoa(secs,istr,10);
+//   strcat(hr_str,istr);
+// }
+
+
+
 /********************************************************
 *
 * Project: MAXREFDES117#
@@ -30,19 +177,13 @@
 * ------------------------------------------------------------------------- */
 
 #include <Arduino.h>
-#include <algorithm_by_RF.h>
-#include <max30102.h>
 #include <SPI.h>
-
-
-// #include <Arduino.h>
-// 
-// #include "algorithm_by_RF.h"
-// #include "max30102.h"
+#include "algorithm_by_RF.h"
+#include "max30102.h"
 
 //#define DEBUG // Uncomment for debug output to the Serial stream
 //#define USE_ADALOGGER // Comment out if you don't have ADALOGGER itself but your MCU still can handle this code
-//#define TEST_MAXIM_ALGORITHM // Uncomment if you want to include results returned by the original MAXIM algorithm
+#define TEST_MAXIM_ALGORITHM // Uncomment if you want to include results returned by the original MAXIM algorithm
 //#define SAVE_RAW_DATA // Uncomment if you want raw data coming out of the sensor saved to SD card. Red signal first, IR second.
 
 #ifdef USE_ADALOGGER
@@ -222,7 +363,6 @@ void loop() {
 
     // change / inverted from Guillaume
     maxim_max30102_read_fifo((aun_ir_buffer + i), (aun_red_buffer + i)); //read from MAX30102 FIFO
-
 
 #ifdef DEBUG
     Serial.print(i, DEC);
